16fcaf73c6d4d3631a7cd421d64b38593a3e5e62
Full Stack Developer <ucberkeley@fulls-air.home>
2021-07-24 17:53:36 -0700

    some DOM

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var formEl = document.getElementById('form');
+// console.log(formEl)
+var historyContainerEl = document.querySelector('.history');
+var cityInputEl = document.getElementById('city');
+
